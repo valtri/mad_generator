d6874529477b86a42e855315d4c1b5d370d6c827
cermak <andrej.cermak.1999@gmail.com>
2020-08-27 14:57:35 +0200

    CHANGED refactor, ADDED docs

--- generator.py
+++ generator.py
@@ -1,19 +1,23 @@
-from random import choices
-from time import time
-from actions import *
+import logging
 import os
 from datetime import datetime
+from math import ceil
+from random import choice, choices, randint
+from time import time
+
 from cloud import CloudRecord
-from storage import StorageRecord
+from events import start_machine, suspend_machine, finish_machine, allocate_ip, free_ip, allocate_cpu, free_cpu, \
+    allocate_memory, free_memory, allocate_storage, free_storage
 from public_ip import PublicIpUsageRecord
-import logging
+from storage import StorageRecord
+
 
 class User:
     count = 0
 
-    def __init__(self):
-        self.user_id = "" + str(User.count)
-        self.group_id = ""
+    def __init__(self, group_id):
+        self.id = str(User.count)
+        self.group_id = str(group_id)
         self.global_name = "GLobalNaMe"
         self.site_name = "goat-vm-site-name"
         self.role = None
@@ -26,17 +30,17 @@ class User:
         for i in range(vm_count):
             cr = CloudRecord()
             cr.set_all({
-                "VMUUID": "user-" + self.user_id + "-machine-" + str(len(self.vms) + 1),
+                "VMUUID": "user-" + self.id + "-machine-" + str(len(self.vms) + 1),
                 "SiteName": self.site_name,
                 "MachineName": "machine" + str(len(self.vms) + 1),
-                "LocalUserId": self.user_id,
+                "LocalUserId": self.id,
                 "LocalGroupId": self.group_id,
                 "GlobalUserName": self.global_name,
                 "CloudComputeService": self.cloud_compute_service,
                 "CloudType": self.cloud_type,
-                "FQAN": "/Group"+ self.group_id +"/Role=NULL/Capability=NULL",
-                "Disk": (2**30)*randint(50, 200),
-                "Memory": (2**30)*randint(1, 16),
+                "FQAN": "/Group" + self.group_id + "/Role=NULL/Capability=NULL",
+                "Disk": (2 ** 30) * randint(50, 200),
+                "Memory": (2 ** 30) * randint(1, 16),
                 "PublicIPCount": randint(0, 2),
                 "IPv4Count": 0,
                 "IPv6Count": 0,
@@ -45,20 +49,26 @@ class User:
                 "WallDuration": 0,
                 "CpuCount": randint(1, 8),
                 "StorageUsage": 0
-                })
+            })
             self.vms.append(cr)
 
     @staticmethod
-    def generate_users(user_count):
+    def generate_users(user_count, group_count):
         users = []
         for i in range(user_count):
-            users.append(User())
+            users.append(User(group_id=randint(1, group_count)))
         return users
 
 
 class Generator:
-    def __init__(self, start_time, cron_interval, event_count, user_count, vm_count_min, vm_count_max, groups_count,
-                 cloud_name, per_file: int, end_time=None):
+    """
+    Generator class that handles everything, that is needed to generate records. Its attributes are self-explanatory.
+    When user wants to generate records, they only have to call public functions.
+
+    """
+
+    def __init__(self, start_time: float, cron_interval: int, event_count: int, user_count: int, vm_count_min: int,
+                 vm_count_max: int, groups_count: int, cloud_name: str, per_file: int, end_time: float = None):
         self.start_time = int(start_time)
         self.cron_interval = cron_interval
         self.event_count = event_count
@@ -85,25 +95,29 @@ class Generator:
 
     def _simulate_cloud_life(self, vm: CloudRecord, events, intervals):
         i, j = 0, 0
-        # TODO pridat logy
-        for i in range (len(events)-1):
+        for i in range(len(events) - 1):
             memory = vm.get_field("Memory")
             cpu = vm.get_field("CpuCount")
             status = vm.get_field("Status")
             ip = vm.get_field("PublicIPCount")
             disk = vm.get_field("StorageUsage")
 
-            while j < len(intervals) and events[i+1] > intervals[j] > events[i]:
-
+            while j < len(intervals) and events[i + 1] > intervals[j] > events[i]:
                 cloud_record = CloudRecord()
                 cloud_record.load_from_msg(vm.get_msg())
 
                 j += 1
+            start_prob = 19
+            suspend_prob = 10
+            if status == "suspended":
+                suspend_prob = 0
+            if status == "started":
+                start_prob = 0
 
-            event = choices([start_machine, finish_machine, suspend_machine, allocate_ip, free_ip, allocate_memory,
+            event = choices([start_machine, suspend_machine, allocate_ip, free_ip, allocate_memory,
                              free_memory, allocate_cpu, free_cpu, allocate_storage, free_storage],
                             k=1,
-                            weights=[19, 1, 10, 19, 19, 19, 19, 19, 19, 19, 19])[0]
+                            weights=[start_prob, suspend_prob, 19, 19, 19, 19, 19, 19, 19, 19])[0]
 
             if memory == 0 and event == free_memory:
                 event = allocate_memory
@@ -118,51 +132,47 @@ class Generator:
             if status is None and (event == finish_machine or event == suspend_machine):
                 event = start_machine
 
-            if status == "suspended" and event == suspend_machine:
-                continue
-            if status == "started" and event == start_machine:
-                continue
-            if status == "completed":
-                continue
-
             vm = event(vm, events[i])
+        if randint(0, 1):
+            finish_machine(vm, self.end_time)
 
-    def _simulate_storage_life(self, user: User, cron_intervals):
+    def _simulate_storage_life(self, user: User, cron_intervals: [int]):
         storages = []
-        from math import ceil
-        for i in range(ceil(self.event_count/self.user_count)):
+
+        for i in range(ceil(self.event_count / self.user_count)):
             start = randint(self.start_time, self.end_time)
             end = randint(start, self.end_time)
-            storages.append((start, end))
+            storages.append({"start_time": start, "end_time": end})
 
         for st in storages:
-            capacity = randint(2**20, 2**30)
-            storage_json = {"RECORD_ID": user.user_id + str(st[0]),  # TODO random generate
-                            "STORAGE_SYSTEM": "ss",  # TODO
+            capacity = randint(2 ** 20, 2 ** 30)
+            storage_json = {"RECORD_ID": user.id + str(st["start_time"]),  # TODO random generate
+                            "STORAGE_SYSTEM": "ss",
                             "STORAGE_SHARE": "datastore" + str(randint(1, 20)),
                             "STORAGE_MEDIA": "disk",
                             "FILE_COUNT": str(randint(1, 100)),
                             "DIRECTORY_PATH": None,
-                            "LOCAL_USER": user.user_id,
+                            "LOCAL_USER": user.id,
                             "LOCAL_GROUP": user.group_id,
-                            "START_TIME": datetime.fromtimestamp(st[0]),  # TODO
+                            "START_TIME": datetime.fromtimestamp(st["start_time"]),
                             "END_TIME": datetime.fromtimestamp(0),
                             "RESOURCE_CAPACITY_USED": capacity,
                             "LOGICAL_CAPACITY_USED": capacity,
                             "RESOURCE_CAPACITY_ALLOCATED": capacity,
                             "GROUP": "/Group" + user.group_id + "/Role=NULL/Capability=NULL ",
                             "STORAGE_CLASS": None,
-                            "USER_IDENTITY": user.global_name,  # todo
-                            "CREATE_TIME": datetime.fromtimestamp(st[0])}
+                            "USER_IDENTITY": user.global_name,
+                            "CREATE_TIME": datetime.fromtimestamp(st["start_time"])}
+
             StorageRecord().set_all(storage_json)
             for interval in cron_intervals:
-                if st[0] <= interval <= st[1]:
-                    storage_json["RECORD_ID"] = user.user_id + str(interval)
+                if st["start_time"] <= interval <= st["end_time"]:
+                    storage_json["RECORD_ID"] = user.id + str(interval)
                     storage_json["CREATE_TIME"] = datetime.fromtimestamp(interval)
                     StorageRecord().set_all(storage_json)
-            storage_json["RECORD_ID"] = user.user_id + str(st[1])
-            storage_json["CREATE_TIME"] = datetime.fromtimestamp(st[1])
-            storage_json["END_TIME"] = datetime.fromtimestamp(st[1])
+            storage_json["RECORD_ID"] = user.id + str(st["end_time"])
+            storage_json["CREATE_TIME"] = datetime.fromtimestamp(st["end_time"])
+            storage_json["END_TIME"] = datetime.fromtimestamp(st["end_time"])
             StorageRecord().set_all(storage_json)
 
     def _simulate_ip_life(self, user, cron_intervals):
@@ -173,64 +183,83 @@ class Generator:
                  "SiteName": user.site_name,
                  "CloudComputeService": user.cloud_compute_service,
                  "CloudType": user.cloud_type,
-                 "LocalUser": user.user_id,
+                 "LocalUser": user.id,
                  "LocalGroup": user.group_id,
                  "GlobalUserName": user.global_name,
-                 "FQAN": "/Group"+user.group_id+"/Role=NULL/Capability=NULL",
+                 "FQAN": "/Group" + user.group_id + "/Role=NULL/Capability=NULL",
                  "IPVersion": choice(["4", "6"]),
                  "IPCount": randint(1, 10)
                  })
 
     def generate_cloud_records(self):
+        """
+            Main function, that handles cloud record generation. Creates CRON and event-triggered records as well.
+
+        """
         cron_intervals = self._generate_cron_intervals()
-        users = User.generate_users(self.user_count)
-        x=0
+        users = User.generate_users(self.user_count, self.groups_count)
+        vm_count = 0
         events_count = 0
         for user in users:
-            user.group_id += str(randint(1, self.groups_count))
             user.cloud_compute_service = self.cloud_name
             user.generate_vms(randint(self.vm_count_min, self.vm_count_max))
-            x+=len(user.vms)
+            vm_count += len(user.vms)
             for vm in user.vms:
-                events = self._generate_event_times()
-                events_count+=len(events)
-                self._simulate_cloud_life(vm, events, cron_intervals)
+                event_times = self._generate_event_times()
+                events_count += len(event_times)
+                self._simulate_cloud_life(vm, event_times, cron_intervals)
+
         self.to_file(CloudRecord, self.per_file)
-        logging.debug("number of cloud records "+str( len(CloudRecord.all_records)))
-        logging.debug("number of vms "+str(x))
-        logging.debug("number of cron generation " +str( len(cron_intervals)))
-        logging.debug("event count "+str( events_count))
+        logging.debug("number of cloud records " + str(len(CloudRecord.all_records)))
+        logging.debug("number of vms " + str(vm_count))
+        logging.debug("number of cron intervals " + str(len(cron_intervals)))
+        logging.debug("event count " + str(events_count))
+
     def generate_storage_records(self):  # TODO
+        """
+            Main function, that handles storage record generation. Creates CRON and event-triggered records as well.
+
+        """
         cron_intervals = self._generate_cron_intervals()
-        users = User.generate_users(self.user_count)
+        users = User.generate_users(self.user_count, self.groups_count)
         for user in users:
-            user.group_id += str(randint(1, self.groups_count))
             user.cloud_compute_service = self.cloud_name
             self._simulate_storage_life(user, cron_intervals)
         self.to_file(StorageRecord, self.per_file)
 
     def generate_ip_records(self):  # TODO check
+        """
+            Main function that handles ip records generation. According to the specification, it creates only
+            records, that were generated by CRON. There are no event triggered records.
+        """
         cron_intervals = self._generate_cron_intervals()
-        users = User.generate_users(self.user_count)
+        users = User.generate_users(self.user_count, self.groups_count)
         for user in users:
-            user.group_id += str(randint(1, self.groups_count))
             user.cloud_compute_service = self.cloud_name
             self._simulate_ip_life(user, cron_intervals)
-
         self.to_file(PublicIpUsageRecord, self.per_file)
 
     def to_file(self, record_class, per_file: int):
+        """
+        Writes generated records to file, accordingly to their format.
+
+        :param record_class: generator mode
+        :param per_file: number of files to be saved in one file
+        :return: None
+        """
         count = 0
         id = 0
-        if not os.path.exists(record_class.__name__+"/"):
-            os.mkdir(record_class.__name__+"/")
-        f = open(record_class.__name__+"/" + "{0:014b}".format(id), "w")
+        if not os.path.exists(record_class.__name__ + "/"):
+            os.mkdir(record_class.__name__ + "/")
+        f = open(record_class.__name__ + "/" + "{0:014b}".format(id), "w")
+
         if record_class == CloudRecord:
             f.write("APEL-cloud-message: v0.4\n")
         if record_class == PublicIpUsageRecord:
             f.write('{"Ips": [\n')
         if record_class == StorageRecord:
             f.write('<?xml version="1.0" encoding="UTF-8"?>\n\n<STORAGES>')
+
         for r in record_class.all_records:
             if count >= per_file:
                 count = 0
